29519ace5976a68408988249c66ea89c0dc33d13
simbionfilm <raditesp@simbionfilm.com>
2026-09-09 02:38:28 +0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './sequence3DData.js';
+import './simbionApp.js';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  createRoot(rootEl).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
